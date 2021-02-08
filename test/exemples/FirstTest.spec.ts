39ff19b4b0312034de00d5bbd1ed84774a867965
Florian <git@florian-a.dev>
2021-02-09 00:41:00 +0100

    Identation fix.

--- test/exemples/FirstTest.spec.ts
+++ test/exemples/FirstTest.spec.ts
@@ -1,9 +1,9 @@
 import { expect } from 'chai'
 
-describe('Exemples :', function() {
-    describe('First test', function() {
-      it('one plus two equal three', function() {
-        expect(1+2).to.equal(3);
-      });
+describe('Exemples :', function () {
+  describe('First test', function () {
+    it('one plus two equal three', function () {
+      expect(1 + 2).to.equal(3);
     });
+  });
 });
