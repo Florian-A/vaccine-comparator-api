923a3c81a1b8ff9d61d35426ac723de80396265d
Florian <git@florian-a.dev>
2021-02-09 00:43:25 +0100

    Database connexion improvement.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import * as express from 'express'
-import { Args } from "./helper/args"
-
+import { database } from './service/Database.service'
 const app = express()
-const port = 3000
+const port = process.env.WEB_PORT
 
-Args.required("env", ["local", "test", "beta", "prod"])
-const ENV = Args.get("env")
-
-console.log(ENV);
+database.query("SELECT * FROM vaccine").then(
+  (res) => {
+    console.log(res)
+  }
+)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listen on -> http://localhost:${port}`)
 })
--- src/service/ databaseConfig.service.ts
+++ src/service/ databaseConfig.service.ts
@@ -1,9 +1,10 @@
-class ConfigService {
-   
-    private host: string = "localhost";
-    private user: string = "me";
-    private password: string = "secret";
-    private database: string = "my_db";
+class DatabaseConfigService {
 
+    public host: string = process.env.DB_HOST;
+    public port: string = process.env.DB_PORT;
+    public user: string = process.env.DB_USER;
+    public password: string = process.env.DB_PASS;
+    public database: string = process.env.DB_NAME;
 }
 
+export const databaseConfig = new DatabaseConfigService()
