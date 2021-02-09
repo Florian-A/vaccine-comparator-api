b94a14b70cdf410c3ae21fa6f33e88bf6fac75ce
Florian <git@florian-a.dev>
2021-02-10 00:45:44 +0100

    Fix bash automated CI bug.

--- sql/test.sh
+++ sql/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cat database.local.sql | mysql -h127.0.0.1 -uroot -p1234
-cat ./test/mockup_db.sql | mysql -h127.0.0.1 -uroot -p1234 vaccine_covid_local
+cat database.test.sql | mysql -h127.0.0.1 -uroot -p1234
+cat structure+dataset.sql | mysql -h127.0.0.1 -uroot -p1234 vaccine_covid_test
