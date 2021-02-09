511bb08cfc3c901d37904b009bf1b1b3dfaa98e4
Raphael Leroudier <leroudier.raphael@gmail.com>
2021-02-09 12:08:15 +0100

    modif vaccin model & creation de laboratory model

---
+++ src/model/laboratory.ts
@@ -0,0 +1,33 @@
+export interface Laboratory {
+    _id: number
+    name: string
+    password: string
+    schema?: object
+}
+
+const Ajv = require('ajv');
+const ajv = Ajv({ allErrors: true });
+
+export class Laboratory {
+    constructor(
+        _id: number = null,
+        name: string,
+        password: string,
+    ) {
+        this._id = _id
+        this.name = name
+        this.password = password
+    }
+    validate() {
+        return (ajv.validate(schema, this)) ? true : ajv.errors
+    }
+}
+const schema = {
+    type: 'object',
+    required: ['name', 'password'],
+    properties: {
+        _id: { type: "number" },
+        name: { type: "string", minLength: 2, maxLength: 255 },
+        password: { type: "string", minLength: 2, maxLength: 16 },
+    }
+}
