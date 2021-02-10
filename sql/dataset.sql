d5ade6ecef491b1c920e87921500f6997c975e84
Florian <git@florian-a.dev>
2021-02-11 00:18:47 +0100

    Add auto-recreate database in test script.

--- sql/dataset.sql
+++ sql/dataset.sql
@@ -1,26 +1,3 @@
-create table if not exists laboratory
-(
-    id       int(9) auto_increment      not null,
-    name     varchar(45)                not null,
-    password varchar(255)               not null,
-    constraint `PRIMARY` primary key (id)
-);
-
-create table if not exists vaccine
-(
-    id             int(9) auto_increment    not null,
-    laboratoryId   int(9)                   null,
-    name           varchar(45)              not null,
-    typeOfVaccine  varchar(45)              null,
-    sideEffects    text(1000)               null,
-    releaseDate    datetime                 null,
-    injection      int(9)                   null,
-    stepValidation varchar(45)              null,
-    constraint `PRIMARY` primary key (id),
-    constraint vaccine_laboratory_id_fk
-        foreign key (laboratoryId) references laboratory (id)
-);
-
 INSERT INTO laboratory (name, password) VALUES ('BioNTech / Pfizer', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
 INSERT INTO laboratory (name, password) VALUES ('NIAID / Moderna', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
 INSERT INTO laboratory (name, password) VALUES ('Oxford / Astra Zeneca', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
