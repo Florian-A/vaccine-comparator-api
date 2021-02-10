bebfb38fb3816a50db6d803af7c789d9b3dd3ad8
Raphael Leroudier <leroudier.raphael@gmail.com>
2021-02-10 14:15:26 +0100

    init router

--- src/service/configService.ts
+++ src/service/configService.ts
@@ -3,14 +3,11 @@ class ConfigService {
     private database: string = "@test";
     private user: string = "@test";
     private password: string = "@test";
-    
 
     initDatabase(){
     //???? à voir quoi faire 
 
     }
-
-
 }
 
 export const config = new ConfigService;
