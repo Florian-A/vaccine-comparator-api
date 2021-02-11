2d787f485ff24e116896e47ade83cc48b3ab0a59
Raphael Leroudier <leroudier.raphael@gmail.com>
2021-02-11 12:07:57 +0100

    test laboratory model ajouté

--- test/model/vaccine.test.spec.ts
+++ test/model/vaccine.test.spec.ts
@@ -8,7 +8,6 @@ describe("Test vaccine model instance.", () => {
     it("new vaccine", () => {
         const vaccine = new Vaccine("H1N1", "vecteur viral", "rougeurs", new Date(), 10000, 'III',1,1)
         const test = vaccine.validate()
-        console.log(test);
         expect(test).to.be.true
     })
 })
